c7555f57d9167436211553ba475b17396042e4d3
sandeep167 <70192923+sandeep167@users.noreply.github.com>
2023-09-13 01:06:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 // config.js
-module.exports = {
-  apiKey: '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', // Replace with your actual API key
-};
+var apiKey = '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; // Replace with your actual API key
 
