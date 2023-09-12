e29a6c6ac287a05db891b722b64245f2ffb38d97
sandeep167 <70192923+sandeep167@users.noreply.github.com>
2023-09-13 01:24:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const config = require('./config'); // Import the config file
+
+app.get('/', (req, res) => {
+  // Read your HTML file
+  const htmlPath = path.join(__dirname, 'index.html');
+  const html = fs.readFileSync(htmlPath, 'utf8');
+
+  // Inject the API key into the HTML
+  const modifiedHtml = html.replace('YOUR_API_KEY_HERE', config.apiKey);
+
+  // Send the modified HTML as the response
+  res.send(modifiedHtml);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
